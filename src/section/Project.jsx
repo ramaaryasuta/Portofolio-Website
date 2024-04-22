e715d6061733efde6c47caec3b31545df3f63349
ramaaryasuta <ramaaryasutaweihan@gmail.com>
2024-04-22 10:36:41 +0700

    add project and alert

---
+++ src/section/Project.jsx
@@ -0,0 +1,12 @@
+import ProjectList from "../components/ProjectList";
+
+export default function ProjectSection() {
+  return (
+    <div className="mx-10 md:mx-10 lg:mx-auto max-w-screen-xl">
+      <h1 className="text-2xl font-bold">My Project :</h1>
+      <br />
+      <ProjectList />
+      <br />
+    </div>
+  );
+}
