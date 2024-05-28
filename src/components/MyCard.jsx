f486203d158cad266764fa186030a8b3f0e0fc09
ramaaryasuta <ramaaryasutaweihan@gmail.com>
2024-05-29 00:12:53 +0700

    fixing project list

--- src/components/MyCard.jsx
+++ src/components/MyCard.jsx
@@ -5,7 +5,7 @@ import { Button } from "@material-tailwind/react";
 const MyCard = ({ title, desc, img, tech, github }) => {
   return (
     <div className="max-w-lg rounded overflow-hidden shadow-lg m-4 flex">
-    <img className="w-1/3 object-cover" src={img} alt="Sample" />
+    <img className="w-1/3 object-cover" src={img} alt="MyProject" />
     <div className="w-2/3 p-4">
       <div className="font-bold text-xl mb-2">{title}</div>
       <p className="text-gray-700 text-base mb-4">
